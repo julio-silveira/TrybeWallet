60c637a1c06f9da7b3636561f7997035f7061473
julio-silveira <julioqueiroz15@gmail.com>
2022-09-29 16:54:40 -0300

    Requisito 02 concluído

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <header>
+        <p
+          data-testid="email-field"
+        >
+          { email }
+        </p>
+        <p
+          data-testid="currency-field"
+        >
+          BRL
+        </p>
+        <p
+          data-testid="total-field"
+        >
+          0
+        </p>
+      </header>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({ email: state.user.email });
+
+export default connect(mapStateToProps, null)(Header);
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -7,7 +7,6 @@ const INITIAL_STATE = {
 function user(state = INITIAL_STATE, action) {
   switch (action.type) {
   case 'ADD_USER':
-    console.log(action);
     return ({ ...state, ...action.value });
   default:
     return state;
